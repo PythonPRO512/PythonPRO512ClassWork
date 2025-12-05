7cbfbe429bd49304ca115594fe17ee5c0f0b6b38
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2025-12-05 21:25:44 +0300

    Урок 15 методы экземпляра, класса, статические.

---
+++ _01_BasicPython/lesson_15_static_class_methods/_01_theory/theory_03.py
@@ -0,0 +1,32 @@
+class Car:
+    wheels = 4
+    cars = []
+
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+        Car.cars.append(self)
+
+    def drive(self):
+        print(f'{self.brand} {self.color} едет!')
+
+    @classmethod
+    def describe(cls):
+        print(f'Все машины данного класса имеют: {cls.wheels} колеса')
+
+    @classmethod
+    def display_all_cars(cls):
+        for car in cls.cars:
+            print(car.brand, car.color)
+
+    @staticmethod
+    def general_info():
+        print(f'Машины это транспортные средства.')
+
+
+if __name__ == '__main__':
+
+    Car.general_info()
+    car1 = Car('Toyota', 'red')
+    car2 = Car('Honda', 'black')
+    Car.display_all_cars()
